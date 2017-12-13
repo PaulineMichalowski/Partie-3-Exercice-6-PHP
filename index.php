4d5143253adb19af78ef5543bedd93e5b4a3a6c5
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:15:29 +0100

    Création de l'exercice 6

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+    $line_number = 20;
+    while($line_number>=0) {
+      echo 'C\'est presque bon ! ';
+      $line_number--;
+    }
+    ?>
+  </body>
+</html>
